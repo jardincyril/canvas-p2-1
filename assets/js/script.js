bea849ef41de3e641728763215a9ad9ec12c2dc3
cyril jardin <cyilroc60@hotmail.fr>
2018-07-24 14:30:05 +0200

    exo1

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var g = document.getElementById('glace')
+var ctx = g.getContext("2d")
+//corne de glace
+ctx.beginPath();
+ctx.moveTo();
+ctx.lineTo();
+ctx.lineTo();
+ctx.closePath();
+ctx.fillStyle = "";
+ctx.fill();
+//boule de glace
+ctx.beginPath();
+ctx.moveTo(,);
+ctx.quadraticCurveTo(220,80,240,280);
+ctx.closePath();
+ctx.fillStyle = "#8A0908";
+ctx.fill();
